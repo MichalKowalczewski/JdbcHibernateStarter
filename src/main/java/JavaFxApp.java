096079f8b5fb65e1098004e1c1315dd2ec1a1d0e
Michał Kowalczewski <michal.kowalczewski@atos.net>
2020-03-26 22:02:41 +0100

    Lekcja1

--- src/main/java/JavaFxApp.java
+++ src/main/java/JavaFxApp.java
@@ -1,16 +1,44 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class JavaFxApp extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(new Pane(), 800,600));
-        stage.show();
-    }
+
+    TableView<Country> table = new TableView();
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    public void start(Stage stage){
+        CountryDAO dao = new CountryDAO();
+        ObservableList<Country> observableList = FXCollections.observableArrayList(dao.getCountries());
+        table.setItems(observableList);
+
+        TableColumn coIdCol = new TableColumn("Country Id");
+        coIdCol.setCellValueFactory(
+                new PropertyValueFactory<Object, String>("countryId"));
+        TableColumn coNameCol = new TableColumn("Country Name");
+        coNameCol.setCellValueFactory(
+                new PropertyValueFactory<Object, String>("countryName"));
+        TableColumn coAliasCol = new TableColumn("Country Alias");
+        coAliasCol.setCellValueFactory(
+                new PropertyValueFactory<Object, String>("countryAlias"));
+
+        table.getColumns().addAll(coIdCol, coNameCol, coAliasCol);
+
+        VBox vBox = new VBox(table);
+        stage.setScene(new Scene(vBox, 800,600));
+        stage.show();
+    }
+
+
 }
